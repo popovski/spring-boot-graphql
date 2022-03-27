09dad97a9b9ff7ab63bad4a7c0d661114a5d2759
Nikola Popovski <nikola.popovski@iwconnect.com>
2022-03-28 00:35:58 +0200

    Rename the data sql script, this file will not be used for inserting new records

--- src/main/resources/data1.sql
+++ src/main/resources/data1.sql
@@ -3,6 +3,7 @@ INSERT INTO application (application_id, app_name, description, owner) VALUES (2
 INSERT INTO application (application_id, app_name, description, owner) VALUES (3, 'Bookings','An application used to book tickets', 'John Doe');
 INSERT INTO application (application_id, app_name, description, owner) VALUES (4, 'Invoice Search','An application used to search invoices ', 'Mary Richards');
 INSERT INTO application (application_id, app_name, description, owner) VALUES (5, 'Audits','An application used for auditing purposes.', 'Tiffany Stewart');
+
 INSERT INTO ticket (id, title, description, application_id, status) VALUES (1, 'Sort Feature','Add the ability to sort tickets by severity',1,'OPEN');
 INSERT INTO ticket (id, title, description, application_id, status) VALUES (2, 'Search Feature','Add the ability to search by invoice date',4,'IN PROGRESS');
 INSERT INTO ticket (id, title, description, application_id, status) VALUES (3, 'Audit','Add the ability to audit by year',5,'CLOSED');
