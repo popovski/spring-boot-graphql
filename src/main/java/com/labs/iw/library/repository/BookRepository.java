66c6707e981afc0e756c82b994e0243d971aa8b6
Nikola Popovski <nikola.popovski@iwconnect.com>
2022-03-28 21:15:36 +0200

    Change BookRepository to extends JpaRepository

--- src/main/java/com/labs/iw/library/repository/BookRepository.java
+++ src/main/java/com/labs/iw/library/repository/BookRepository.java
@@ -3,9 +3,9 @@ package com.labs.iw.library.repository;
 import com.labs.iw.library.entity.Book;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
-	public Iterable<Book> findAll(Pageable pageable);
-	public List<Book> findAll();
+public interface BookRepository extends JpaRepository<Book, Long>, CrudRepository<Book, Long> {
+
 }
